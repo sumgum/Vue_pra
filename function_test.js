34167c966d1785494c23a4291132ed96669eb34f
Sumi Gunyu <sumkakkyo@gmail.com>
2020-07-09 19:44:22 +0900

    Vueの配列変更メソッド確認

---
+++ function_test.js
@@ -0,0 +1,28 @@
+const animals = ['pigs', 'goats', 'sheeps']
+
+// push 配列の末尾に1つ以上の要素を追加する。戻り値は新しい配列の要素数
+const PUSH = animals.push('cows');
+console.log("PUSH", PUSH);
+console.log("PUSH", animals);
+
+// pop 配列の最後の要素を取り除き、その要素を返す
+const POP = animals.pop();
+console.log("POP", POP);
+console.log("POP", animals);
+
+// shift 配列から最初の要素を取り除き、その要素を返します。
+const SHIFT = animals.shift();
+console.log("SHIFT", SHIFT);
+console.log("SHIFT", animals);
+
+// unshift 配列の最初に１個以上の要素を追加し、配列の変更後の長さを返します
+const UNSHIFT = animals.unshift('pigs', 'dogs');
+console.log("UNSHIFT", UNSHIFT);
+console.log("UNSHIFT", animals);
+
+// reverse
+const REVERSE = animals.reverse();
+console.log("REVERSE", REVERSE);
+console.log("REVERSE", animals);
+
+
