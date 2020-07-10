9309247461c9312b30d65c35bb8ab0bb83f2595a
Sumi Gunyu <sumkakkyo@gmail.com>
2020-07-10 18:08:18 +0900

    for確認

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
-    items: [],
+    items: [
+      {index: 1, value: "a"},
+      {index: 2, value: "b"},
+      {index: 3, value: "c"},
+      {index: 4, value: "d"},
+      {index: 5, value: "e"},
+      {index: 6, value: "f"},
+      {index: 7, value: "g"},
+    ],
   }
 })
