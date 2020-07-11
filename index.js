af2584c88dfcdef8e43fbdc4b9a7e8f820113373
Sumi Gunyu <sumkakkyo@gmail.com>
2020-07-11 20:36:39 +0900

    Vue.js 2.xドキュメントガイドみながら確認(１周完了)

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+Vue.component('base-layout', {
+  template: `
+  <div class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+  `
+})
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -11,11 +27,22 @@ var app = new Vue({
       {index: 6, value: "f"},
       {index: 7, value: "g"},
     ],
-    numbers: [1, 2, 3, 4, 5]
+    numbers: [1, 2, 3, 4, 5],
+    sets: [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10]
+    ],
   },
   computed: {
     eventNumbers: function() {
       return this.numbers.filter(function(number) {
+        return number % 2 === 0
+      })
+    }
+  },
+  methods: {
+    even: function(numbers) {
+      return numbers.filter(function(number) {
         return number % 2 === 0
       })
     }
